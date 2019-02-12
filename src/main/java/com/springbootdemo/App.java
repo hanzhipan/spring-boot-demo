acaff61692a24011425c313f1551a8281f2e858a
hanzhipan <HUANGhongdan0823>
2019-02-12 14:46:30 +0800

    集成actuator

--- src/main/java/com/springbootdemo/App.java
+++ src/main/java/com/springbootdemo/App.java
@@ -2,17 +2,19 @@ package com.springbootdemo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class App {
     public static void main(String[] args) {
         new SpringApplicationBuilder(App.class).web(true).run(args);
